dc38f7b22753ff90cba96c036c494006481a7560
MonthAlcantara <montival_junior@yahoo.com.br>
2025-05-14 20:31:57 -0300

    feat: add separate files for route handlers

--- router/routes.go
+++ router/routes.go
@@ -6,22 +6,21 @@ import (
 )
 
 func initializeRoutes(router *gin.Engine) {
-	// Definindo uma rota GET /ping
+
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", handler.GetOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
 	}
 	{
 		v1.POST("/opening", handler.CreateOpeningHandler)
 	}
 	{
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
-
 	}
 	{
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 	}
 	{
-		v1.GET("/openings", handler.GetAllOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
